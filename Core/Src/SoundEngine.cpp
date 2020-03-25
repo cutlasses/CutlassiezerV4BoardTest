fdcd097a83ef6e4838a7d0ab8a3f27e0468eb564
Cutlasses <spitkethly@gmail.com>
2020-03-25 18:19:12 +0000

    Implementing audio pass-through

--- Core/Src/SoundEngine.cpp
+++ Core/Src/SoundEngine.cpp
@@ -1,10 +1,10 @@
+#include <memory.h>
+
 #include "DebugOutput.h"
 #include "main.h"
 #include "Oscillator.h"
 #include "SoundEngine.h"
 #include "WM8731.h"
-
-#define FILL_AUDIO_IN_INTERRUPT
 
 const constexpr uint16_t SAMPLE_RATE(44100);
 const constexpr uint16_t AUDIO_BLOCK_SIZE(128);
@@ -12,83 +12,76 @@ const constexpr uint16_t NUM_TRANSMIT_BLOCKS(2);	// double buffered
 const constexpr uint16_t NUM_CHANNELS(2);			// left and right
 const constexpr uint16_t TRANSMIT_BUFFER_SIZE(AUDIO_BLOCK_SIZE*NUM_TRANSMIT_BLOCKS*2);
 const constexpr uint16_t RECEIVE_BUFFER_SIZE(AUDIO_BLOCK_SIZE*NUM_TRANSMIT_BLOCKS*2);
+const constexpr uint16_t HALF_TRANSMIT_BUFFER_SIZE(TRANSMIT_BUFFER_SIZE/2);
 
 int16_t transmit_buffer[TRANSMIT_BUFFER_SIZE];
 int16_t receive_buffer[RECEIVE_BUFFER_SIZE];
 
-Oscillator<int16_t, SAMPLE_RATE> sine_osc;
+volatile int16_t audio_buffer_left[AUDIO_BLOCK_SIZE];
+volatile bool receive_complete = false;
+
+void audio_stream_receive( const int16_t* buffer, size_t size )
+{
+	receive_complete = false;
+
+	// strip the left channel only
+	for( size_t i = 0; i < size; i+=2 )
+	{
+		audio_buffer_left[i] = buffer[i];
+	}
+
+	// process audio
+
+	receive_complete = true;
+}
+
+void audio_stream_transmit( int16_t* buffer, size_t size )
+{
+	// interleave left and right (currently left only)
+	ASSERT_MSG( receive_complete, "Receive interrupt interrupted?" );
+
+	for( size_t i = 0; i < size; i+=2 )
+	{
+		buffer[i] = audio_buffer_left[i];
+		buffer[i+1] = 0;
+	}
+}
 
 // TRANSMIT - transfer half complete
 void HAL_SAI_TxHalfCpltCallback(SAI_HandleTypeDef *hsai)
 {
-#ifdef FILL_AUDIO_IN_INTERRUPT
 	// start writing from the beginning
-	const int16_t start_write_block = 0;
-	const int16_t end_write_block = TRANSMIT_BUFFER_SIZE / 2;
-
-	for( int16_t si = start_write_block; si < end_write_block; si+=2 )
-	{
-		const int16_t sample	= sine_osc.next_sample();
-		transmit_buffer[si]		= sample;
-		transmit_buffer[si+1]	= sample;
-	}
-#endif
+	audio_stream_transmit( &(transmit_buffer[0]), HALF_TRANSMIT_BUFFER_SIZE );
 }
 
 // TRANSMIT - transfer complete
 void HAL_SAI_TxCpltCallback(SAI_HandleTypeDef *hsai)
 {
-#ifdef FILL_AUDIO_IN_INTERRUPT
 	// start writing from midpoint
-	const int16_t start_write_block = TRANSMIT_BUFFER_SIZE / 2;
-	const int16_t end_write_block = TRANSMIT_BUFFER_SIZE;
+	audio_stream_transmit( &(transmit_buffer[HALF_TRANSMIT_BUFFER_SIZE]), HALF_TRANSMIT_BUFFER_SIZE );
 
-	for( int16_t si = start_write_block; si < end_write_block; si+=2 )
-	{
-		const int16_t sample	= sine_osc.next_sample();
-		transmit_buffer[si]		= sample;
-		transmit_buffer[si+1]	= sample;
-	}
-#endif
 }
 
 // RECEIVE - receive half complete
 void HAL_SAI_RxHalfCpltCallback(SAI_HandleTypeDef *hsai)
 {
-
+	audio_stream_receive( &(receive_buffer[0]), HALF_TRANSMIT_BUFFER_SIZE );
 }
 
 // RECEIVE - receive complete
 void HAL_SAI_RxCpltCallback(SAI_HandleTypeDef *hsai)
 {
-
+	audio_stream_receive( &(receive_buffer[HALF_TRANSMIT_BUFFER_SIZE]), HALF_TRANSMIT_BUFFER_SIZE );
 }
 
 void initialise_sound_engine()
 {
 	DEBUG_TEXT("initialise_sound_engine()\n");
 
-#ifndef FILL_AUDIO_IN_INTERRUPT
-	// fill transmit buffer with sine wave
-	const constexpr float buffer_time = (1.0f / SAMPLE_RATE) * AUDIO_BLOCK_SIZE * 2;
-	const constexpr float buffer_freq = 1.0f / buffer_time;
-	sine_osc.set_frequency(buffer_freq);
-
-	for( int16_t si = 0; si < TRANSMIT_BUFFER_SIZE; si+=2 )
-	{
-		const int16_t sample	= sine_osc.next_sample();
-		transmit_buffer[si]		= sample;
-		transmit_buffer[si+1]	= sample;
-	}
-#else
 	// reset the transmit buffer
-	for( int16_t& sample : transmit_buffer )
-	{
-		sample = 0;
-	}
+	memset( transmit_buffer, 0, TRANSMIT_BUFFER_SIZE );
+	memset( const_cast<int16_t*>(audio_buffer_left), 0, AUDIO_BLOCK_SIZE );
 
-	sine_osc.set_frequency(441.0f);
-#endif
 
 	// start off DMA
 	if( HAL_OK != HAL_SAI_Transmit_DMA(&hsai_BlockB1, (uint8_t*)transmit_buffer, TRANSMIT_BUFFER_SIZE ) )
